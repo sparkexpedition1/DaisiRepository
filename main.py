eb41b525e15cddfc8ff82d9cb79018e110341b16
sparkexpedition1 <61966148+sparkexpedition1@users.noreply.github.com>
2022-07-30 13:47:20 +0530

    Update main.py

--- main.py
+++ main.py
@@ -106,23 +106,20 @@ def uiExample():
     st.markdown('DOW Jones :  ' + str(dj_cur) + ' Change: ' + str(dj_change) + '%')
     
     #fig = plt.figure()
-    l1 = [["Nifty",nifty_change],["Sensex", sensex_change], ["SNP",snp_change], ["Russel",russel_change], ["DOW Jones",dj_change]]
-    #l2 = ['Nifty', 'Sensex', 'S&P',"Russel",'DOW Jones']
-#     df_chart = pd.DataFrame(l1,columns=["Index","Change"])
-#     st.dataframe(data=df_chart)
-    lp =[]
-    ln = []
-    for lst in l1:
-        if lst[1] >= 0 :
-            lp.append(lst)
-        else:
-            ln.append(lst)
-    df_lp  =  pd.DataFrame(lp,columns=["Index","Change"])
-    df_ln  =  pd.DataFrame(ln,columns=["Index","Change"])
+#     l1 = [["Nifty",nifty_change],["Sensex", sensex_change], ["SNP",snp_change], ["Russel",russel_change], ["DOW Jones",dj_change]]
+#     lp =[]
+#     ln = []
+#     for lst in l1:
+#         if lst[1] >= 0 :
+#             lp.append(lst)
+#         else:
+#             ln.append(lst)
+#     df_lp  =  pd.DataFrame(lp,columns=["Index","Change"])
+#     df_ln  =  pd.DataFrame(ln,columns=["Index","Change"])
     
-    plt.plot(df_lp['Index'],df_lp['Change'],type="bar",color="green")
-    plt.plot(df_ln['Index'],df_ln['Change'],type="bar",color="red")
-    st.pyplot(fig=plt)
+#     plt.plot(df_lp['Index'],df_lp['Change'],type="bar",color="green")
+#     plt.plot(df_ln['Index'],df_ln['Change'],type="bar",color="red")
+#     st.pyplot(fig=plt)
 #     plt.show()
 #     nsei =yf.Ticker('NSEI')
 #     st.text(nsei.info)
